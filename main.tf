4362750a2f0ef0c983f369c4cefb6f367fc89eb6
KumarNeelabhSingh <96595375+KumarNeelabhSingh@users.noreply.github.com>
2024-07-31 09:11:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,32 @@
 provider "aws" {
- 
-  region = "us-east-1"
+
+  
+
+ region = "us-east-1"
+
+
+
 }
+
 
 variable "keyname" {
 
  type = string
 
 }
+
+
+
+terraform {
+  backend "s3" {
+      region = "us-east-1"
+     bucket = "nts18s3tf"
+     dynamodb_table = "tfnts18"
+     key = "terraform.tfstate"
+    
+  }
+}
+
 
 
 
@@ -35,6 +54,66 @@ resource "local_file" "tf-key" {
 
 content = tls_private_key.rsa.private_key_pem
 
-filename = var.keyname
+filename = var.tls_private_key
+
+}
+
+
+
+
+
+resource "aws_instance" "web-server" {
+
+ ami      = "ami-04a81a99f5ec58529"
+
+ instance_type = "t2.micro"
+
+ key_name   = var.tls_private_key
+
+
+
+
+
+ 
+
+ provisioner "remote-exec" {
+
+   inline = [
+
+        "sudo apt-get update",
+
+		"sudo apt-get update",
+
+		"sudo apt install -y apache2",
+
+		"sudo chmod -R 777 /var/www/html/"
+
+	
+
+     
+
+   ]
+
+  }
+
+	
+
+ provisioner "file" {
+
+  source   = "index.html"
+
+  destination = "/var/www/html/index.html"
+
+ }
+
+ connection {
+
+  user    = "ubuntu"
+
+  private_key = "${file(local_file.tf-key.filename)}"
+
+   host = "${aws_instance.web-server.public_ip}"
+
+ }
 
 }
